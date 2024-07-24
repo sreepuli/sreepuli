02a656b00a19ca6c3a2338a2047bd837de18febd
sreepuli <166621160+sreepuli@users.noreply.github.com>
2024-07-24 11:49:13 +0530

    Add files via upload

---
+++ defaultvalue.java
@@ -0,0 +1,20 @@
+public class defaultvalue{
+	static byte defaultByte;
+	static short defaultShort;
+	static int defaultInt;
+	static long defaultLong;
+	static float defaultFloat;
+	static double defaultDouble;
+	static char defaultChar;
+	static boolean defaultboolean;
+	public static void main(String[] args){
+	 System.out.println("default Byte= " + defaultByte);
+	 System.out.println("default Short= " + defaultShort);
+	 System.out.println("default Int= " + defaultInt);
+	 System.out.println("default Long= " + defaultLong);
+	 System.out.println("default Float= " + defaultFloat);
+	 System.out.println("default Double= " + defaultDouble);
+	 System.out.println("default Char=  " + defaultChar);
+	 System.out.println("default Boolean= " + defaultboolean);
+	}
+}
