d29a360880f58ea270c7529d306461535a1e8cf9
sreepuli <166621160+sreepuli@users.noreply.github.com>
2024-09-18 11:13:56 +0530

    Add files via upload

--- main.java
+++ main.java
@@ -1,6 +1,14 @@
-public class main{
-	public static void main(String[] args){
-		Book book=new Book("Effective.java","Joshua bloch",45.00);
-		book.displayInfo();
+ class Car extends Vehicle{
+	public void start(){
+	System.out.println("Car started");
 	}
 }
+public class main{
+	public static void main(String[] args){
+	Vehicle obj=new Vehicle();
+	obj.start();
+	Car car= new Car();
+	car.start();
+	}
+}
+
